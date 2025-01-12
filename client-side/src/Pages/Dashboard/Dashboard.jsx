09d30c62ef5bba26adac2d84e5e47661c7d801ee
coderskamrul <mdkamrul2058@gmail.com>
2025-01-12 22:57:12 +0600

    Dashboard Content Updated

--- client-side/src/Pages/Dashboard/Dashboard.jsx
+++ client-side/src/Pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import TaskBoard from './TaskBoard/TaskBoard'
 import useProjects from '../../Hooks/useProjects';
+import useUsers from '../../Hooks/useUsers';
 
 const Dashboard = () => {
     const [projects, isProjectLoading, reFetchProject] = useProjects();
+    const [users, usersLoading] = useUsers();
     // Calculate total number of tasks
     const totalTasks = projects.reduce((projectAcc, project) => {
         return projectAcc + project.column.reduce((columnAcc, column) => {
@@ -35,11 +37,11 @@ const Dashboard = () => {
                 </div>
                 <div className="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
                     <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-                        <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
+                    <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
                     </div>
                     <div className="text-right">
-                        <p className="text-2xl">$11,257</p>
-                        <p>Sales</p>
+                        <p className="text-2xl">{users.length}</p>
+                        <p>Total Users</p>
                     </div>
                 </div>
                 <div className="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
@@ -47,8 +49,8 @@ const Dashboard = () => {
                         <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                     </div>
                     <div className="text-right">
-                        <p className="text-2xl">$75,257</p>
-                        <p>Balances</p>
+                        <p className="text-2xl">00</p>
+                        <p>Notifications</p>
                     </div>
                 </div>
             </div>
